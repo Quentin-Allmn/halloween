602a274a8b5343ab2472e3d74d57e982b6262c26
Quentin-Allmn <92529322+Quentin-Allmn@users.noreply.github.com>
2021-11-08 15:15:43 +0100

    m

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -410,7 +410,7 @@ class Tableau1 extends Phaser.Scene{
         //initialise ce qui se passe avec le clavier
         this.initKeyboard();
         // Définit l'espace de déplacement de la caméra
-        this.cameras.main.setBounds(0, 0, 1800, 540);
+        this.cameras.main.setBounds(0, 0, 2000, 540);
         //définit à quelles vitesse se déplacent nos différents plans
         bgAnimationA.scrollFactorX=0;
         this.filterFilm.scrollFactorX=0;
